57a5659e003ab50ce6320bef7c81aa7f43f90ad6
Hugo <hugolacerdar@gmail.com>
2021-10-15 20:43:50 -0300

    Add routes

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,10 +1,15 @@
+import "reflect-metadata";
+
 import express, { Request, Response } from "express";
 
 import createConnection from "../typeorm";
+import { router } from "./routes";
+import "../../container";
 
 createConnection();
 const app = express();
 
 app.use(express.json());
+app.use(router);
 
 export { app };
