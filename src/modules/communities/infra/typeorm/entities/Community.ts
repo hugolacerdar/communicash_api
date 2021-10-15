7dde6e41760f59ec4978ad2a1478c9062c7ff6a8
Hugo <hugolacerdar@gmail.com>
2021-10-15 20:39:38 -0300

    Add community entity

---
+++ src/modules/communities/infra/typeorm/entities/Community.ts
@@ -0,0 +1,37 @@
+import { User } from "../../../../accounts/infra/typeorm/entities/User";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryColumn,
+} from "typeorm";
+import { v4 as uuid4 } from "uuid";
+
+@Entity("communities")
+class Community {
+  @PrimaryColumn()
+  id: string;
+
+  @OneToOne(() => User)
+  @JoinColumn({ name: "creator_id" })
+  creator_id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid4();
+    }
+  }
+}
+
+export { Community };
