e8936521daf60b966eefaf8d5a4a20aa30e561a8
Hugo <hugolacerdar@gmail.com>
2021-10-21 22:50:16 -0300

    Update adding other crud routes and auth middleware

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,11 +1,25 @@
 import { Router } from "express";
 
+import { GetUserProfileController } from "../../../../modules/accounts/useCases/getUserProfile/GetUserProfileController";
 import { CreateUserController } from "../../../../modules/accounts/useCases/createUser/CreateUserController";
+import { DeleteUserController } from "../../../../modules/accounts/useCases/deleteUser/DeleteUserController";
+import { EditUserController } from "../../../../modules/accounts/useCases/editUser/EditUserController";
+import { checkAuthentication } from "../middlewares/checkAuthentication";
 
 const userRoutes = Router();
 
 const createUserController = new CreateUserController();
+const getUserProfileController = new GetUserProfileController();
+const deleteUserController = new DeleteUserController();
+const editUserController = new EditUserController();
 
 userRoutes.post("/", createUserController.handle);
+userRoutes.get(
+  "/profile",
+  checkAuthentication,
+  getUserProfileController.handle
+);
+userRoutes.delete("/profile", checkAuthentication, deleteUserController.handle);
+userRoutes.put("/profile", checkAuthentication, editUserController.handle);
 
 export { userRoutes };
