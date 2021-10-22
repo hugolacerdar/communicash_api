4beef42481d39d44a5194fc667c4f9de2d543944
Hugo <hugolacerdar@gmail.com>
2021-10-21 22:47:23 -0300

    Add date provider

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,25 @@
 import { container } from "tsyringe";
 
+import "./providers";
+
 import { UsersRepository } from "../../modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+import { UsersTokensRepository } from "../../modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
+import { IUsersTokensRepository } from "../../modules/accounts/repositories/IUsersTokensRepository";
+import { CommunitiesRepository } from "../../modules/communities/infra/typeorm/repositories/CommunitiesRepository";
+import { ICommunitiesRepository } from "../../modules/communities/repositories/ICommunitiesRepository";
 
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
+);
+
+container.registerSingleton<IUsersTokensRepository>(
+  "UsersTokensRepository",
+  UsersTokensRepository
+);
+
+container.registerSingleton<ICommunitiesRepository>(
+  "CommunitiesRepository",
+  CommunitiesRepository
 );
