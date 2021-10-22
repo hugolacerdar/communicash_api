cfe1fa1ebb8f858680d5e6dabf958cf3de83d3b3
Hugo <hugolacerdar@gmail.com>
2021-10-21 22:49:19 -0300

    Add community and authentication routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { userRoutes } from "./users.routes";
+import { authenticationRoutes } from "./authentication.routes";
+import { communityRoutes } from "./communities.routes";
 
 const router = Router();
 
 router.use("/users", userRoutes);
+router.use("/auth", authenticationRoutes);
+router.use("/communities", communityRoutes);
 
 export { router };
