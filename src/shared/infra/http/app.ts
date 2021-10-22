9809d5ce41aa2055d14f251fdf80b282beabac45
Hugo <hugolacerdar@gmail.com>
2021-10-21 22:43:24 -0300

    Add error handling logic

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,15 +1,29 @@
 import "reflect-metadata";
 
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 import createConnection from "../typeorm";
 import { router } from "./routes";
 import "../../container";
+import { AppError } from "../../error/AppError";
 
 createConnection();
 const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({ message: err.message });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 export { app };
