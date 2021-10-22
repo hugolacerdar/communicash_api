7114feb955cc4056807fb4805435b9a8915f3739
Hugo <hugolacerdar@gmail.com>
2021-10-21 22:42:48 -0300

    Add error handling

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -1,22 +1,30 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { container } from "tsyringe";
 
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
-  async handle(request: Request, response: Response): Promise<Response> {
-    const { full_name, email, password, birth_date } = request.body;
+  async handle(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ): Promise<Response> {
+    try {
+      const { full_name, email, password, birth_date } = request.body;
 
-    const createUserUseCase = container.resolve(CreateUserUseCase);
+      const createUserUseCase = container.resolve(CreateUserUseCase);
 
-    await createUserUseCase.execute({
-      full_name,
-      email,
-      password,
-      birth_date,
-    });
+      await createUserUseCase.execute({
+        full_name,
+        email,
+        password,
+        birth_date,
+      });
 
-    return response.status(201).send();
+      return response.status(201).send();
+    } catch (error) {
+      next(error);
+    }
   }
 }
 
