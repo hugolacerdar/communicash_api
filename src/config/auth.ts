8320807be12ab70c729970c5ace4b1bbf9c02149
Hugo <hugolacerdar@gmail.com>
2021-10-21 22:37:02 -0300

    Add auth tokens config

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  tokenSecret: "836f1eaaa2e2e4bc6e33cabf47ab6b9a",
+  refreshTokenSecret: "00216857dac58da21780a5b6d2644495",
+  tokenExpiresIn: "1d",
+  refreshTokenExpiresIn: "30d",
+  refreshTokenExpiresInNum: 30,
+};
