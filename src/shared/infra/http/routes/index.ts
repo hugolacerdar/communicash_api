66f2aa3caad9798fafe0266c5fd6c6d8f77c1c8c
Hugo <hugolacerdar@gmail.com>
2021-10-26 22:03:16 -0300

    Add income router

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { userRoutes } from "./users.routes";
 import { authenticationRoutes } from "./authentication.routes";
 import { communityRoutes } from "./communities.routes";
+import { incomeRoutes } from "./incomes.routes";
 
 const router = Router();
 
 router.use("/users", userRoutes);
 router.use("/auth", authenticationRoutes);
 router.use("/communities", communityRoutes);
+router.use("/incomes", incomeRoutes);
 
 export { router };
