25e7ce40a95c4e2f7d4741959888b384ddf80cb4
Hugo <hugolacerdar@gmail.com>
2021-10-26 22:01:54 -0300

    Add checkIsCommunityMember middleware

---
+++ src/shared/infra/http/middlewares/checkIsCommunityMember.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+
+import { AppError } from "../../../../shared/error/AppError";
+import { UsersRepository } from "../../../../modules/accounts/infra/typeorm/repositories/UsersRepository";
+
+export async function checkIsCommunityMember(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  try {
+    const { user_id } = request;
+    const usersRepository = new UsersRepository();
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user.community_id) {
+      throw new AppError("No community association found.");
+    }
+
+    request.community_id = user.community_id;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
