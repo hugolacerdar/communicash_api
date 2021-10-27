93b37e3fca3d356249a226bf9b8c12fdc9a24538
Hugo <hugolacerdar@gmail.com>
2021-10-26 21:59:45 -0300

    Add add create income controller

---
+++ src/modules/incomes/useCases/createIncome/CreateIncomeController.ts
@@ -0,0 +1,28 @@
+import { IIncomesRepository } from "../../repositories/IIncomesRepositories";
+import { container, inject, injectable } from "tsyringe";
+import { ICreateIncomeDTO } from "../../dtos/ICreateIncomeDTO";
+import { NextFunction, Request, Response } from "express";
+import { CreateIncomeUseCase } from "./CreateIncomeUseCase";
+
+class CreateIncomeController {
+  async handle(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ): Promise<Response> {
+    try {
+      const { user_id } = request;
+      const { amount, description, date } = request.body;
+
+      const createIncomeUseCase = container.resolve(CreateIncomeUseCase);
+
+      await createIncomeUseCase.execute({ user_id, amount, description, date });
+
+      return response.status(204).send();
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export { CreateIncomeController };
