5fe4744417059e41df6e3b7f32a701b3fc3cd555
Hugo <hugolacerdar@gmail.com>
2021-10-26 22:02:09 -0300

    Add add member route

--- src/shared/infra/http/routes/communities.routes.ts
+++ src/shared/infra/http/routes/communities.routes.ts
@@ -1,16 +1,26 @@
 import { Router } from "express";
 
+import { AddMemberController } from "../../../../modules/communities/useCases/addMember/AddMemberController";
 import { CreateCommunityController } from "../../../../modules/communities/useCases/createCommunity/CreateCommunityController";
 import { checkAuthentication } from "../middlewares/checkAuthentication";
+import { checkIsCommunityMember } from "../middlewares/checkIsCommunityMember";
 
 const communityRoutes = Router();
 
 const createCommunityController = new CreateCommunityController();
+const addMemberController = new AddMemberController();
 
 communityRoutes.post(
   "/",
   checkAuthentication,
   createCommunityController.handle
+);
+
+communityRoutes.post(
+  "/members",
+  checkAuthentication,
+  checkIsCommunityMember,
+  addMemberController.handle
 );
 
 export { communityRoutes };
