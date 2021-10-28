31314d9115fe0ff3468002fef9b4ed00e0f680da
Hugo <hugolacerdar@gmail.com>
2021-10-28 15:54:00 -0300

    Add get full community endpoint

--- src/shared/infra/http/routes/communities.routes.ts
+++ src/shared/infra/http/routes/communities.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { AddMemberController } from "../../../../modules/communities/useCases/addMember/AddMemberController";
 import { CreateCommunityController } from "../../../../modules/communities/useCases/createCommunity/CreateCommunityController";
+import { GetIncomesAndExpensesController } from "../../../../modules/communities/useCases/getIncomesAndExpenses/GetIncomesAndExpensesController";
 import { checkAuthentication } from "../middlewares/checkAuthentication";
 import { checkIsCommunityMember } from "../middlewares/checkIsCommunityMember";
 
@@ -9,11 +10,19 @@ const communityRoutes = Router();
 
 const createCommunityController = new CreateCommunityController();
 const addMemberController = new AddMemberController();
+const getIncomesAndExpenses = new GetIncomesAndExpensesController();
 
 communityRoutes.post(
   "/",
   checkAuthentication,
   createCommunityController.handle
+);
+
+communityRoutes.get(
+  "/",
+  checkAuthentication,
+  checkIsCommunityMember,
+  getIncomesAndExpenses.handle
 );
 
 communityRoutes.post(
