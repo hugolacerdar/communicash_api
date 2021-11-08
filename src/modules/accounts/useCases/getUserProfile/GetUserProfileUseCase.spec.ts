096f568f8fbe790b8daf1a2fc020f18fdefbd9ab
Hugo <hugolacerdar@gmail.com>
2021-11-08 17:49:11 -0300

    Add Get User Profile Use Case tests

---
+++ src/modules/accounts/useCases/getUserProfile/GetUserProfileUseCase.spec.ts
@@ -0,0 +1,35 @@
+import { v4 as uuid4 } from "uuid";
+
+import { InMemoryUsersRepository } from "../../repositories/inMemory/UsersRepository";
+import { AppError } from "../../../../shared/error/AppError";
+
+import { GetUserProfileUseCase } from "./GetUserProfileUseCase";
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let getUserProfileUseCase: GetUserProfileUseCase;
+
+describe("Get User Profile", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    getUserProfileUseCase = new GetUserProfileUseCase(inMemoryUsersRepository);
+  });
+
+  it("should be able to get the user", async () => {
+    const id = uuid4();
+
+    const data = {
+      full_name: "Banana Test",
+      email: "banana@test.com",
+      password: "12345",
+      birth_date: new Date("1999-12-12 00:00:00"),
+      id: id,
+    };
+
+    await inMemoryUsersRepository.create(data);
+
+    const user = await getUserProfileUseCase.execute(id);
+
+    expect(user).toHaveProperty("id");
+    expect(user.id).toEqual(id);
+  });
+});
