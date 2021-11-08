615784e27ad0bcb677b3d59e857a5ef0f8bb6c85
Hugo <hugolacerdar@gmail.com>
2021-11-08 16:56:19 -0300

    Add Create User Controller tests

--- src/modules/accounts/useCases/createUser/CreateUserController.spec.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.spec.ts
@@ -13,4 +13,31 @@ describe("Create User Controller", () => {
     connection = await createConnection();
     await connection.runMigrations();
   });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("should be able to create a new user", async () => {
+    const response = await request(app).post("/users").send({
+      full_name: "Supertest Banana",
+      password: "123456",
+      email: "supertest@example.com",
+      birth_date: "2000-12-12",
+    });
+
+    expect(response.status).toBe(201);
+  });
+
+  it("should not be able to create a new user with an email already in use on the platform", async () => {
+    const response = await request(app).post("/users").send({
+      full_name: "Supertest Banana",
+      password: "123456",
+      email: "supertest@example.com",
+      birth_date: "2000-12-12",
+    });
+
+    expect(response.status).toBe(400);
+  });
 });
