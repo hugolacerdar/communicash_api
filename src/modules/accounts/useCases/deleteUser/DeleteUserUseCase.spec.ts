c628973a45bc8ed0088065cb09d82295044eb708
Hugo <hugolacerdar@gmail.com>
2021-11-09 09:18:09 -0300

    Add Delete User Use Case tests

---
+++ src/modules/accounts/useCases/deleteUser/DeleteUserUseCase.spec.ts
@@ -0,0 +1,40 @@
+import { v4 as uuid4 } from "uuid";
+
+import { InMemoryUsersRepository } from "../../repositories/inMemory/UsersRepository";
+import { AppError } from "../../../../shared/error/AppError";
+
+import { DeleteUserUseCase } from "./DeleteUserUseCase";
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let deleteUserUseCase: DeleteUserUseCase;
+
+describe("Delete User Use Case", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    deleteUserUseCase = new DeleteUserUseCase(inMemoryUsersRepository);
+  });
+
+  it("should be able to delete a user", async () => {
+    const id = uuid4();
+
+    const data = {
+      full_name: "Banana Test",
+      email: "banana@test.com",
+      password: "12345",
+      birth_date: new Date("1999-12-12 00:00:00"),
+      id: id,
+    };
+
+    await inMemoryUsersRepository.create(data);
+
+    const beforeDeleteUser = await inMemoryUsersRepository.findById(id);
+
+    expect(beforeDeleteUser.id).toBe(id);
+
+    await deleteUserUseCase.execute(id);
+
+    const afterDeleteUser = await inMemoryUsersRepository.findById(id);
+
+    expect(afterDeleteUser).toBe(undefined);
+  });
+});
