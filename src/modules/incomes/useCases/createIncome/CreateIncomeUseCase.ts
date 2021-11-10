731b150fe3dbf61a221da176f2ae8fe736a67963
Hugo <hugolacerdar@gmail.com>
2021-11-09 21:03:20 -0300

    Update Create Income Use Case

--- src/modules/incomes/useCases/createIncome/CreateIncomeUseCase.ts
+++ src/modules/incomes/useCases/createIncome/CreateIncomeUseCase.ts
@@ -4,18 +4,12 @@ import { inject, injectable } from "tsyringe";
 import { AppError } from "../../../../shared/error/AppError";
 import { IIncomesRepository } from "../../repositories/IIncomesRepository";
 import { ICreateIncomeDTO } from "../../dtos/ICreateIncomeDTO";
-import { IUsersRepository } from "../../../accounts/repositories/IUsersRepository";
-import { ICommunitiesRepository } from "../../../communities/repositories/ICommunitiesRepository";
 
 @injectable()
 class CreateIncomeUseCase {
   constructor(
     @inject("IncomesRepository")
-    private incomesRepository: IIncomesRepository,
-    @inject("UsersRepository")
-    private usersRepository: IUsersRepository,
-    @inject("CommunitiesRepository")
-    private communitiesRepository: ICommunitiesRepository
+    private incomesRepository: IIncomesRepository
   ) {}
 
   async execute({
@@ -25,14 +19,6 @@ class CreateIncomeUseCase {
     date,
     community_id,
   }: ICreateIncomeDTO) {
-    const user = await this.usersRepository.findById(user_id);
-
-    if (!user) throw new AppError("User not found.");
-
-    const community = await this.communitiesRepository.findById(community_id);
-
-    if (!community) throw new AppError("Community not found.");
-
     await this.incomesRepository.create({
       user_id,
       amount,
